422ff225f3e5425aebe22b8ad61ce7143aa79f41
Vijay Chand <vijaychand6979@googlemail.com>
2026-03-30 16:11:50 +0100

    Added code

--- app/main.py
+++ app/main.py
@@ -1,1 +1,15 @@
-# write your code here
+def copy_file(command: str) -> None:
+    if len(command.split()) != 3:
+        return
+
+    cmd, file_in, file_out = command.split()
+
+    if file_in != file_out and cmd == "cp":
+        try:
+            with (
+                open(file_in, "r") as in_file,
+                open(file_out, "w") as out_file
+            ):
+                out_file.writelines(in_file.readlines())
+        except FileNotFoundError as e:
+            print(e)
